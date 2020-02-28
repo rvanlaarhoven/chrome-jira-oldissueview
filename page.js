16e9e35c18ade16769ec082d972fe01feb529100
Rick van Laarhoven <hello@rickvanlaarhoven.nl>
2020-02-28 08:41:54 +0100

    Remove debug commands

--- page.js
+++ page.js
@@ -1,7 +1,4 @@
-console.log('_____DOCUMENTTTT______', document);
-
 const editButton = document.getElementById('edit-issue');
 if (!editButton) {
-  console.log('Edit button not loaded');
+  editButton.click();
 }
-editButton.click();
